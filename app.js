6935d9c7ee6d5932eaa8601cf78fa72f51517d4f
shaunmoini <primary.axos@dralias.com>
2021-09-28 05:08:40 -0400

    Implemented 'order' functionality on server, no web client ui.

--- app.js
+++ app.js
@@ -9,12 +9,27 @@ const db_url = 'mongodb://localhost:27017/';
 app.use(express.json());
 app.set('view engine', 'pug');
 
+// get html for for adding new product
+app.get('/addproduct', (req, res) => {
+  res.render('addProduct');
+})
+
+// get html for for adding new review
+app.get('/addreview', (req, res) => {
+  res.render('addReview');
+})
+
+// get html for for placing order
+app.get('/placeorder', (req, res) => {
+  res.render('placeOrder');
+})
+
 //default path 
 app.get('/', (req, res) => {
   mongoClient.connect(db_url, function(err, client) {
     if (err) throw err;
     var dbo = client.db('gamazon');
-    
+
     dbo.collection('products').find({}).toArray(function(err, result) {
       if (err) throw err;
       res.render('app', {products: result});
@@ -45,22 +60,25 @@ app.get('/products/:id', (req, res) => {
 
     dbo.collection('products').findOne({id: parseInt(req.params.id)}, function(err, result) {
       if (err) throw err;
-      var product = result;
 
-      if(req.headers.accept && req.headers.accept.includes('text/html')) {
-        res.render('product', {
-          name: product.name,
-          price: product.price,
-          dimensions: 'dimensions',
-          x: product.dimensions.x,
-          y: product.dimensions.y,
-          z: product.dimensions.z,
-          stock: product.stock,
-          id: product.id,
-          reviews: product.reviews ? product.reviews : 'No reviews.'
-        })
+      if(result != null) {
+        if(req.headers.accept && req.headers.accept.includes('text/html')) {
+          res.render('product', {
+            name: result.name,
+            price: result.price,
+            dimensions: 'dimensions',
+            x: result.dimensions.x,
+            y: result.dimensions.y,
+            z: result.dimensions.z,
+            stock: result.stock,
+            id: result.id,
+            reviews: result.reviews ? result.reviews : 'No reviews.'
+          })
+        } else {
+          res.status(200).json(result);
+        }
       } else {
-        res.status(200).json(product);
+        res.status(400).json('Invalid product id');
       }
 
       client.close();
@@ -76,15 +94,18 @@ app.get('/products/:id/reviews', (req, res) => {
 
     dbo.collection('products').findOne({id: parseInt(req.params.id)}, function(err, result) {
       if (err) throw err;
-      var product = result;
 
-      if(req.headers.accept && req.headers.accept.includes('text/html')) {
-        res.render('reviews', {
-          id: product.id,
-          reviews: product.reviews ? product.reviews : 'No reviews.'
-        })
+      if(result != null) {
+        if(req.headers.accept && req.headers.accept.includes('text/html')) {
+          res.render('reviews', {
+            id: result.id,
+            reviews: result.reviews ? result.reviews : 'No reviews.'
+          })
+        } else {
+          res.status(200).json({id: result.id, reviews: result.reviews ? result.reviews : []});
+        }
       } else {
-        res.status(200).json({id: product.id, reviews: product.reviews ? product.reviews : []});
+        res.status(400).json('Invalid product id');
       }
 
       client.close();
@@ -124,14 +145,36 @@ app.get('/search', (req, res) => {
   });
 });
 
-// get html for for adding new product
-app.get('/addproduct', (req, res) => {
-  res.render('addProduct');
+app.get('/orders', (req, res) => {
+  mongoClient.connect(db_url, function(err, client) {
+    if (err) throw err;
+    var dbo = client.db('gamazon');
+
+    dbo.collection('orders').find({}).toArray(function(err, result) {
+      if (err) throw err;
+      res.status(200).json(result);
+      client.close();
+    });
+  });
 })
 
-// get html for for adding new review
-app.get('/addreview', (req, res) => {
-  res.render('addReview');
+app.get('/orders/:id', (req, res) => {
+  mongoClient.connect(db_url, function(err, client) {
+    if (err) throw err;
+    var dbo = client.db('gamazon');
+
+    dbo.collection('orders').findOne({id: parseInt(req.params.id)}, function(err, result) {
+      if (err) throw err;
+
+      if(result != null) {
+        res.status(200).json(result);
+      } else {
+        res.status(400).json('Invalid order id');
+      }
+
+      client.close();
+    });
+  });
 })
 
 // add new product
@@ -140,7 +183,7 @@ app.post(
   validator.body('name').isString(),
   validator.body('price').isNumeric(),
   validator.body('dimensions').isObject(),
-  validator.body('stock').isNumeric(),
+  validator.body('stock').isInt(),
   (req, res) => {
     const errors = validator.validationResult(req);
     if(!errors.isEmpty()) {
@@ -208,6 +251,95 @@ app.post(
     }); 
   }
 );
+
+app.post(
+  '/orders',
+  validator.body('customerName').notEmpty().isString(),
+  validator.body('order.*.product_id').notEmpty().isInt(),
+  validator.body('order.*.quantity').notEmpty().isInt(),
+  (req, res) => {
+    const errors = validator.validationResult(req);
+    if(!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    mongoClient.connect(db_url, function(err, client) {
+      if (err) throw err;
+      var dbo = client.db('gamazon');
+  
+      dbo.collection('products').find({}).toArray(function(err, result) {
+        if (err) throw err;
+        var reqValid = true;
+        var invalidResStr = [];
+
+        for(x of req.body.order) {
+          var item = result.find((product) => {
+            return x.product_id == product.id;
+          })
+          
+          if(item){
+            if(x.quantity <= item.stock) {
+              continue;
+            } else {
+              reqValid = false;
+              invalidResStr.push(`product id ${x.product_id}: not enough stock`)
+            }
+          } else {
+            reqValid = false;
+            invalidResStr.push(`invalid product id: ${x.product_id}`)
+          }
+        }
+
+        if(reqValid) {
+          for(product of req.body.order) {
+            product.product_link = `http://localhost:3000/products/${product.product_id}`;
+          }
+          dbo.collection('orders').countDocuments().then((count) => {
+            var order = {
+              id: count,
+              customerName: req.body.customerName,
+              order: req.body.order,
+              link: `http://localhost:3000/orders/${count}`
+            }
+    
+            dbo.collection('orders').insertOne(order, function(err, result) {
+              if (err) throw err;
+              console.log(result);
+              updateStock(req.body.order);
+              res.status(201).json('order created successfully');
+              client.close();
+            });
+          })
+        } else {
+          res.status(409).json(invalidResStr);
+          client.close();
+        }
+      });
+    });
+  }
+)
+
+function updateStock(order) {
+  mongoClient.connect(db_url, function(err, client) {
+    if (err) throw err;
+    var dbo = client.db('gamazon');
+
+    for(let i=0; i < order.length; i++) {
+      dbo.collection('products').findOne({id: parseInt(order[i].product_id)}, function(err, result) {
+        if (err) throw err;
+        var newValue = newValue = {$set: {stock: (result.stock - order[i].quantity)}};
+  
+        dbo.collection('products').updateOne({id: parseInt(order[i].product_id)}, newValue, function(err, result) {
+          if (err) throw err;
+          console.log(result);
+          if(i == order.length-1) {
+            client.close();
+          }
+        });
+      })
+    }
+  })
+}
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
